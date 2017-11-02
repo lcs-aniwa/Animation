679d1d44a608b66a5042ca522ae27b3c445ccc98
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2017-11-02 13:31:16 -0400

    Helloween image

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -14,8 +14,64 @@
 import Cocoa
 import PlaygroundSupport
 
+
+
+/*:
+ ## Add your code below
+ 
+ Be sure to write human-readable code.
+ 
+ Use whitespace and comments as appropriate.
+ */
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 800, height: 600)
+
+// No borders
+canvas.drawShapesWithBorders = false
+
+// Draw the axes so we can see where we are
+canvas.drawAxes()
+
+// Move the origin to the middle of the canvasand
+canvas.translate(byX: 400, byY: 300)
+
+
+
+// Draw a bat relative to the origin
+canvas.fillColor = Color.orange
+
+
+// First make an ellipse to form the body
+canvas.drawEllipse(centreX: 0, centreY: 0, width: 300, height: 200)
+
+
+// Underside of the botom part
+canvas.fillColor = Color.orange
+canvas.drawEllipse(centreX: -60, centreY: -70, width: 80, height: 80) // left
+canvas.drawEllipse(centreX: 0, centreY: -70, width: 80, height: 80) // left middle
+canvas.drawEllipse(centreX: 70, centreY: -70, width: 80, height: 80) // right middle
+ // eyes
+
+
+
+/*:
+ ## Use source control
+ To keep your work organized, and receive feedback, source control is a must.
+ 
+ Please commit and push your work often.
+ 
+ ![source_control](source_control.png "Source Control")
+ */
+/*:
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right.
+ 
+ Please do not remove.
+ 
+ If you don't see output, remember to show the Assistant Editor, and switch to Live View:
+ 
+ ![timeline](timeline.png "Timeline")
+ */
 
 /*:
  ## Add your code below
