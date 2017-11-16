b950217cb932ae01b6285fe225b24e992cc5905b
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2017-11-15 21:47:52 -0500

    madman symbol

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -1,107 +1,58 @@
-//: [Previous](@previous) / [Next](@next)
-//: # Test Area
-//:
-//: Use this page to experiment with a static image.
-//:
-//: When you perfect a technique, you can move it into the animated sketch.
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
+
 import Cocoa
 import PlaygroundSupport
 
-
-
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
 // Create canvas
 let canvas = Canvas(width: 800, height: 600)
 
-// No borders
 canvas.drawShapesWithBorders = false
+
+// Move the prigin to the middle of the canvasand
+canvas.translate(byX: 400, byY: 300)
 
 // Draw the axes so we can see where we are
 canvas.drawAxes()
 
-// Move the origin to the middle of the canvasand
-canvas.translate(byX: 400, byY: 300)
-
-
-
 // Draw a bat relative to the origin
-canvas.fillColor = Color.orange
+canvas.fillColor = Color.black
 
+// FIrst make an ellipse to form the body
+canvas.drawEllipse(centreX: 0, centreY: 0, width: 200, height: 100)
 
-// First make an ellipse to form the body
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 300, height: 200)
+// Now "cut out" the wings and head by overlapping circles
+canvas.fillColor = Color.white
 
-
-// Underside of the botom part
-canvas.fillColor = Color.orange
-canvas.drawEllipse(centreX: -60, centreY: -70, width: 80, height: 80) // left
-canvas.drawEllipse(centreX: 0, centreY: -70, width: 80, height: 80) // left middle
-canvas.drawEllipse(centreX: 70, centreY: -70, width: 80, height: 80) // right middle
- // eyes
-
-
-
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
-
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// Underside of wings
+canvas.drawEllipse(centreX: -70, centreY: -10, width: 50, height: 50)
+canvas.drawEllipse(centreX: -35, centreY: -10, width: 50, height: 50)
+canvas.drawEllipse(centreX: 35, centreY: -10, width: 50, height: 50)
+canvas.drawEllipse(centreX: 70, centreY: -10, width: 50, height: 50)
+canvas.drawEllipse(centreX: 0, centreY: -25, width: 50, height: 50)
+canvas.drawEllipse(centreX: 0, centreY: -40, width: 150, height: 50)
+// Now add the head
+canvas.fillColor = Color.black
+var headVertices : [NSPoint] = []
+headVertices.append(NSPoint(x: -40, y: 40))
+headVertices.append(NSPoint(x: 40, y: 40))
+headVertices.append(NSPoint(x: 30, y: 80))
+headVertices.append(NSPoint(x: 20, y: 60))
+headVertices.append(NSPoint(x: -20, y: 60))
+headVertices.append(NSPoint(x: -30, y: 80))
+headVertices.append(NSPoint(x: -40, y: 40))
 
 
 
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
+
+
+
+
+// End where we started
+canvas.drawCustomShape(with: headVertices)
+
+
+
+
+
 // Don't remove the code below
 PlaygroundPage.current.liveView = canvas.imageView
 
